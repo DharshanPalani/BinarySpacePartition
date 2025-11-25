000166791e3e2b706503c615348066c34d229716
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-11-25 13:22:15 +0530

    Joe

--- public/dist/main.js
+++ public/dist/main.js
@@ -10,24 +10,21 @@ const leaf = [root];
 const min_leaf_size = Math.floor(Math.random() * (20 - 10 + 1) + 15);
 let didSplit = true;
 while (didSplit) {
-    didSplit = false;
-    for (let i = 0; i < leaf.length; i++) {
-        const leaves = leaf[i];
-        if (leaves.left !== null && leaves.right !== null)
-            continue;
-        if (leaves.width < min_leaf_size || leaves.height < min_leaf_size)
-            continue;
-        if (!split(leaves, min_leaf_size))
-            continue;
-        leaf.push(leaves.left);
-        leaf.push(leaves.right);
-        didSplit = true;
-    }
+  didSplit = false;
+  for (let i = 0; i < leaf.length; i++) {
+    const leaves = leaf[i];
+    if (leaves.left !== null && leaves.right !== null) continue;
+    if (leaves.width < min_leaf_size || leaves.height < min_leaf_size) continue;
+    if (!split(leaves, min_leaf_size)) continue;
+    leaf.push(leaves.left);
+    leaf.push(leaves.right);
+    didSplit = true;
+  }
 }
 for (const l of leaf) {
-    if (l.left === null && l.right === null) {
-        createRoom(l, 8, 20);
-    }
+  if (l.left === null && l.right === null) {
+    createRoom(l, 8, 20);
+  }
 }
 const finalLeaves = leaf.filter((l) => l.left === null && l.right === null);
 console.log(finalLeaves);
